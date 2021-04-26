f329c4a0f78258658f6018e5583748c7e78c3729
jeffostrin <jeff.ostrin@gmail.com>
2021-04-26 09:05:52 -0700

    Inspect Account 1 Event Types
    Inspect Account 1 Event Types

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 addEventListener("fetch", (event) => {
   event.respondWith(
+    
+    let result = await nrql('show event types')
+    let counts = tally(result.results[0].eventTypes)
+    console.log(sort(counts))
+  
     new Response("Hello Ward and Jeff", {
       status: 200,
       headers: {
@@ -8,3 +13,34 @@ addEventListener("fetch", (event) => {
     }),
   );
 });
+
+// fetch and analyze event types
+// usage: ACCT_1_INSIGHTS_QUERY_KEY='...' deno run --allow-env --allow.net fetch.js
+function nrql(query) {
+  let site = `https://staging-insights-api.newrelic.com`
+  let route = `v1/accounts/1/query`
+  let url = `${site}/${route}?nrql=${query}`
+  let headers = {
+    "Accept": "application/json",
+    "X-Query-Key": Deno.env.get("ACCT_1_INSIGHTS_QUERY_KEY")
+  }
+  return fetch(url, {headers}).then(res => res.ok ? res.json() : console.log(res.statusText))
+}
+function tally(types) {
+  let counts = {}
+  for (let type of types) {
+    for (let word of type.split(' ')) {
+      for (let snake of word.split('_')) {
+        for (let camel of snake.split(/(?<=[a-z])(?=[A-Z])/)) {
+          counts[camel] = counts[camel] || 0
+          counts[camel] += 1
+        }
+      }      
+    }
+  }
+  return counts
+}
+function sort(counts) {
+  let array = Object.entries(counts)
+  return array.sort((a,b) => b[1]-a[1])
+}
